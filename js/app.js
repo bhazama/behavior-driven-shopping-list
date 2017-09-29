9eac3ab675e3bbc8cd4c6edcf99ecb947c51acc4
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-28 19:58:32 -1000

    working on check status

--- js/app.js
+++ js/app.js
@@ -9,3 +9,13 @@ content.innerHTML = listItem;
 
 let title = document.getElementById("title");
 let description = document.getElementById("description");
+
+function add_to_shopping_list(){
+  let new_shopping_list_item = new ShoppingListitem(title.value, description.value);
+  list.addItem(new_shopping_list_item);
+  content.innerHTML = list.render();
+}
+
+function changeCheckedStatus(idx, checkbox){
+
+}
